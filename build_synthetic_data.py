32d9536d1bc6e2d2ee6e6aaaf7179aeccde39991
Extracheesy <desportes.cedric@gmail.com>
2022-10-28 15:12:52 +0200

    ___

--- build_synthetic_data.py
+++ build_synthetic_data.py
@@ -51,40 +51,81 @@ def add_df_ohlv_noise(df, amplitude):
     return df
 
 def build_synthetic_data(interval):
-    df_synthetic = pd.DataFrame(columns=['time', 'sinus_1', 'sinus_2', 'linear'])
+    df_synthetic = pd.DataFrame(columns=['time', 'sinus_1', 'sinus_2', 'linear_up', 'linear_down'])
 
     df_synthetic = fill_df_date(df_synthetic, interval[0], interval[1])
 
-    freq = 10 / len(df_synthetic)
-    # freq = 10 / len(df_synthetic) / 3.2
-    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_1', config.sinus_1_amplitude, freq, 0, config.sinus_1_height)
+    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_1', config.sinus_1_amplitude, config.sinus_1_freq, 0, config.sinus_1_height)
 
-    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_2', config.sinus_2_amplitude, config.sinus_2_freq, 0, config.sinus_2_height)
+    freq = 6.3 / len(df_synthetic)
+    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_2', config.sinus_2_amplitude, freq, 0, config.sinus_2_height)
 
-    df_synthetic = fill_df_linear(df_synthetic, 'linear', config.a, config.b)
-    if config.NO_LINEAR:
-        df_synthetic['linear'] = 0
+    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_3', config.sinus_3_amplitude, config.sinus_3_freq, 0, config.sinus_3_height)
 
-    df_ohlv = pd.DataFrame(columns=['time', 'close'])
-    for columns in df_synthetic.columns.tolist():
-        if columns == 'time':
-            df_ohlv['time'] = df_synthetic['time']
-        else:
-            df_ohlv.fillna(0, inplace=True)
-            df_ohlv['close'] = df_ohlv['close'] + df_synthetic[columns]
+    freq = 6.3 / len(df_synthetic) / 2
+    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_4', config.sinus_4_amplitude, freq, 0, config.sinus_4_height)
+
+    freq = 6.3 / len(df_synthetic) / 2
+    df_synthetic = fill_df_sinusoid(df_synthetic, 'sinus_5', config.sinus_5_amplitude, freq, 0, config.sinus_5_height)
+    df_synthetic['sinus_5'] = 1 - df_synthetic['sinus_5']
+
+    df_synthetic = fill_df_linear(df_synthetic, 'linear_up', config.a, config.b)
+
+    df_synthetic = fill_df_linear(df_synthetic, 'linear_down', -config.a, config.b)
 
     plot_data.plot_df_sinus(df_synthetic, 'sinus_1')
     plot_data.plot_df_sinus(df_synthetic, 'sinus_2')
-    plot_data.plot_df_sinus(df_synthetic, 'linear')
-    plot_data.plot_df_sinus(df_ohlv, 'close')
+    plot_data.plot_df_sinus(df_synthetic, 'sinus_3')
+    plot_data.plot_df_sinus(df_synthetic, 'sinus_4')
+    plot_data.plot_df_sinus(df_synthetic, 'sinus_5')
+    plot_data.plot_df_sinus(df_synthetic, 'linear_up')
+    plot_data.plot_df_sinus(df_synthetic, 'linear_down')
 
-    fill_ohlv(df_ohlv)
-    filename = 'ohlc_no_noise'
-    plot_data.plot_ohlc(df_ohlv, filename)
-    df_ohlv.to_csv(config.OUTPUT_DIRECTORY + 'ohlc_no_noise.csv')
+    df_ohlv = pd.DataFrame(columns=['time', 'close'])
 
-    amplitude = 0.1
-    df_ohlv = add_df_ohlv_noise(df_ohlv, amplitude)
-    filename = 'ohlc_with_noise'
-    plot_data.plot_ohlc(df_ohlv, filename)
-    df_ohlv.to_csv(config.OUTPUT_DIRECTORY + 'ohlc_with_noise.csv')
+    for type in config.TYPE:
+        if type == 'SINGLE_SINUS_1_FLAT':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_1'] + 10
+        if type == 'SINGLE_SINUS_2_FLAT':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + 10
+        if type == 'MIXED_SINUS_FLAT':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + df_synthetic['sinus_3'] + 10
+        if type == 'SINGLE_SINUS_1_UP':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_1'] + df_synthetic['linear_up'] + 10
+        if type == 'SINGLE_SINUS_2_UP':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + df_synthetic['linear_up'] + 10
+        if type == 'MIXED_SINUS_UP':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + df_synthetic['sinus_3'] + df_synthetic['linear_up'] + 10
+        if type == 'SINGLE_SINUS_1_DOWN':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_1'] + df_synthetic['linear_down'] + 10
+        if type == 'SINGLE_SINUS_2_DOWN':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + df_synthetic['linear_down'] + 10
+        if type == 'MIXED_SINUS_DOWN':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_2'] + df_synthetic['sinus_3'] + df_synthetic['linear_down'] + 10
+        if type == 'MIXED_SINUS_UP_DOWN':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_3'] + df_synthetic['sinus_4'] + 10
+        if type == 'MIXED_SINUS_DOWN_UP':
+            df_ohlv['time'] = df_synthetic['time']
+            df_ohlv['close'] = df_synthetic['sinus_3'] + df_synthetic['sinus_5'] + 10
+
+
+        fill_ohlv(df_ohlv)
+        filename = 'ohlc_no_noise_' + type
+        plot_data.plot_ohlc(df_ohlv, filename)
+        df_ohlv.to_csv(config.OUTPUT_DIRECTORY + filename + '.csv')
+
+        amplitude = 0.1
+        df_ohlv = add_df_ohlv_noise(df_ohlv, amplitude)
+        filename = 'ohlc_with_noise_' + type
+        plot_data.plot_ohlc(df_ohlv, filename)
+        df_ohlv.to_csv(config.OUTPUT_DIRECTORY + filename + '.csv')
--- config.py
+++ config.py
@@ -7,12 +7,35 @@ a = 0.001
 b = 10
 
 # parameter sinus 1
+sinus_1_freq = 0.1
 sinus_1_amplitude = 1
-sinus_1_height = 10
+sinus_1_height = 0
 
 # parameter sinus 2
-sinus_2_freq = 0.1
-sinus_2_amplitude = 0.3
+sinus_2_amplitude = 1
 sinus_2_height = 0
 
+sinus_3_freq = 0.1
+sinus_3_amplitude = 0.3
+sinus_3_height = 0
+
+sinus_4_amplitude = 1
+sinus_4_height = 0
+
+sinus_5_amplitude = 1
+sinus_5_height = 0
+
 CRAG_ANALYSER = False
+
+TYPE = ['SINGLE_SINUS_1_FLAT',
+        'SINGLE_SINUS_2_FLAT',
+        'MIXED_SINUS_FLAT',
+        'SINGLE_SINUS_1_UP',
+        'SINGLE_SINUS_2_UP',
+        'MIXED_SINUS_UP',
+        'SINGLE_SINUS_1_DOWN',
+        'SINGLE_SINUS_2_DOWN',
+        'MIXED_SINUS_DOWN',
+        'MIXED_SINUS_UP_DOWN',
+        'MIXED_SINUS_DOWN_UP'
+        ]
